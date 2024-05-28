668294d6ac24a0ec4b4a7cd66577150750951699
Rares Petruc <raresp@4psa.me>
2024-05-28 13:57:36 +0300

    testing collection

--- target_qdrant/sinks.py
+++ target_qdrant/sinks.py
@@ -29,7 +29,8 @@ class QdrantSink(BatchSink):
         super().__init__(target, stream_name, schema, key_properties)
         
         self.collection = self.config["collection"]
-        
+        self.collection = "testulet"
+
         endpoint = self.config["endpoint"]
         port = self.config["port"]
         self.qdrant_client = QdrantClient(url=endpoint, port=port)
